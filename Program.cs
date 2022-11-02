499d428c5d682896a05670481be62a584ce0830f
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-02 12:26:58 +0300

    Simple middleware

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
+using System.Net.Mime;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == HttpMethods.Get
+        && context.Request.Query["custom"] == "true")
+    {
+        //HttpRequest httpRequest = context.Request;
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("Custom Middleware \n");
+    }
+    await next.Invoke();
+});
+
+app.MapGet("/", async (context) =>
+{
+    //context.Response.StatusCode = 202;
+    await context.Response.WriteAsync("MapGet \n");
+});
 
 app.Run();
