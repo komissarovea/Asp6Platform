3441b340ced78039c8adb50b8f24e023eb041601
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-02 16:15:45 +0300

    add class-based middleware

---
+++ Middleware.cs
@@ -0,0 +1,26 @@
+﻿namespace Platform
+{
+    public class QueryStringMiddleWare
+    {
+        private RequestDelegate next;
+
+        public QueryStringMiddleWare(RequestDelegate nextDelegate)
+        {
+            this.next = nextDelegate;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+            {
+                if (!context.Response.HasStarted)
+                {
+                    context.Response.ContentType = "text/plain";
+                }
+                await context.Response.WriteAsync("Class-based Middleware \n");
+            }
+            await next(context);
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-using System.Net.Mime;
+using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Use(async (context, next) =>
 {
-    if (context.Request.Method == HttpMethods.Get
-        && context.Request.Query["custom"] == "true")
+    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
     {
-        //HttpRequest httpRequest = context.Request;
         context.Response.ContentType = "text/plain";
         await context.Response.WriteAsync("Custom Middleware \n");
     }
     await next.Invoke();
 });
+
+app.UseMiddleware<QueryStringMiddleWare>();
 
 app.MapGet("/", async (context) =>
 {
