6219d69b5dd8b18f328c491553e15640d5418c7f
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-04 01:49:22 +0300

    fallback endpoint

--- Program.cs
+++ Program.cs
@@ -13,22 +13,37 @@ var app = builder.Build();
 //    }
 //});
 
-//app.MapGet("{firs:alpha:length(3)}/{secon:bool}", async context => {
-//    await context.Response.WriteAsync("Request Was Routed\n");
-//    foreach (var kvp in context.Request.RouteValues)
-//    {
-//        await context.Response
-//        .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-//    }
-//});
+app.MapGet("{firs:alpha:length(3)}/{secon:bool}", async context =>
+{
+    await context.Response.WriteAsync("Request Was Routed\n");
+    foreach (var kvp in context.Request.RouteValues)
+    {
+        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+    }
+});
 
 //app.MapGet("capital/{country=France}", Capital.Endpoint);
 app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
 app.MapGet("size/{city?}", Population.Endpoint2).WithMetadata(new RouteNameMetadata("population2"));
 
-app.MapGet(String.Empty, async (HttpContext context) =>
+//app.MapFallback(async context =>
+//{
+//    await context.Response.WriteAsync("Routed to fallback endpoint\n");
+
+//    foreach (var kvp in context.Request.RouteValues)
+//    {
+//        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+//    }
+//});
+
+app.MapGet("{*smth}", async context =>
 {
-    await context.Response.WriteAsync("Default \n");
+    await context.Response.WriteAsync("Routed to fallback endpoint\n");
+
+    foreach (var kvp in context.Request.RouteValues)
+    {
+        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+    }
 });
 
 app.Run();
