e95ffec4c3c39f19db78f157494fec3f6b3c5f2d
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-03 23:48:09 +0300

    simplified routing

--- Program.cs
+++ Program.cs
@@ -1,27 +1,20 @@
-using Microsoft.Extensions.Options;
 using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-//app.UseMiddleware<Population>();
-//app.UseMiddleware<Capital>();
 
-app.UseRouting();
-
-app.UseEndpoints(endpoints =>
+app.MapGet("routing", async context =>
 {
-    endpoints.MapGet("routing", async context =>
-    {
-        await context.Response.WriteAsync("Request Was Routed");
-    });
-    endpoints.MapGet("capital/uk", new Capital().Invoke);
-    endpoints.MapGet("population/paris", new Population().Invoke);
+    await context.Response.WriteAsync("Request Was Routed");
 });
+app.MapGet("capital/uk", new Capital().Invoke);
+app.MapGet("population/paris", new Population().Invoke);
 
-app.Run(async (context) =>
-{
-    await context.Response.WriteAsync("Terminal Middleware Reached");
-});
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Terminal Middleware Reached");
+//});
 
 app.Run();
