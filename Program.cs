15a56e3a4801f7db890f6ac41f1d44c6c7df1920
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-04 02:06:04 +0300

    custom constrain

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<RouteOptions>(opts =>
+{
+    opts.ConstraintMap.Add("countryName",  typeof(CountryRouteConstraint));
+});
 
 var app = builder.Build();
 
-//app.MapGet("{first:int}/{second:bool}", async context =>
+//app.MapGet("{firs:alpha:length(3)}/{secon:bool}", async context =>
 //{
 //    await context.Response.WriteAsync("Request Was Routed\n");
 //    foreach (var kvp in context.Request.RouteValues)
@@ -13,30 +17,12 @@ var app = builder.Build();
 //    }
 //});
 
-app.MapGet("{firs:alpha:length(3)}/{secon:bool}", async context =>
-{
-    await context.Response.WriteAsync("Request Was Routed\n");
-    foreach (var kvp in context.Request.RouteValues)
-    {
-        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-    }
-});
-
-//app.MapGet("capital/{country=France}", Capital.Endpoint);
-app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
+app.MapGet("capital/{country:countryName}", Capital.Endpoint);
+//app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
 app.MapGet("size/{city?}", Population.Endpoint2).WithMetadata(new RouteNameMetadata("population2"));
 
-//app.MapFallback(async context =>
-//{
-//    await context.Response.WriteAsync("Routed to fallback endpoint\n");
 
-//    foreach (var kvp in context.Request.RouteValues)
-//    {
-//        await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-//    }
-//});
-
-app.MapGet("{*smth}", async context =>
+app.MapGet("{*path}", async context =>
 {
     await context.Response.WriteAsync("Routed to fallback endpoint\n");
 
