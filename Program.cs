fd6ec3756220139845fc4bad53f18bd031759b0c
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-04 01:25:47 +0300

    default segment variables

--- Program.cs
+++ Program.cs
@@ -4,25 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("{first}/{second}/{third}", async context =>
+app.MapGet("files/{filename}.{ext}", async context =>
 {
-    await context.Response.WriteAsync("Request Was Routed 3\n");
-    foreach (var kvp in context.Request.RouteValues)
-    {
-        await context.Response .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-    }
-});
-
-app.MapGet("{first}/{second}", async context =>
-{
-    await context.Response.WriteAsync("Request Was Routed 2\n");
+    await context.Response.WriteAsync("Request Was Routed\n");
     foreach (var kvp in context.Request.RouteValues)
     {
         await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
     }
 });
 
-app.MapGet("capital/{country}", Capital.Endpoint);
+app.MapGet("capital/{country=France}", Capital.Endpoint);
 app.MapGet("size/{city}", Population.Endpoint2).WithMetadata(new RouteNameMetadata("population2"));
 
 app.MapGet(String.Empty, async (HttpContext context) =>
