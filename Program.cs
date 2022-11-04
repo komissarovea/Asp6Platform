120c3d4680023df32d93f6c89c3841555969b74f
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-04 18:33:03 +0300

    services start

--- Program.cs
+++ Program.cs
@@ -1,40 +1,21 @@
 using Platform;
+using Platform.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<RouteOptions>(opts =>
-{
-    opts.ConstraintMap.Add("countryName", typeof(CountryRouteConstraint));
-});
-
 var app = builder.Build();
 
-app.Map("{number:double}", async context =>
-{
-    await context.Response.WriteAsync("Routed to the double endpoint");
-}).WithDisplayName("Double Endpoint").Add(b => ((RouteEndpointBuilder)b).Order = 2);
+app.UseMiddleware<WeatherMiddleware>();
 
-app.Use(async (HttpContext context, Func<Task> next) =>
-{
-    Endpoint? end = context.GetEndpoint();
-    if (end != null)
-    {
-        await context.Response.WriteAsync($"{end.DisplayName} Selected \n");
-        await next();
-    }
-    else
-    {
-        await context.Response.WriteAsync("No Endpoint Selected \n");
-    }
+
+IResponseFormatter formatter = new TextResponseFormatter();
+app.MapGet("middleware/function", async (context) => {
+    await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
 });
 
-app.Map("{number:int}", async context =>
-{
-    await context.Response.WriteAsync("Routed to the int endpoint");
-}).WithDisplayName("Int Endpoint").Add(b => ((RouteEndpointBuilder)b).Order = 1);
+app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
 
-app.MapFallback(async context =>
-{
-    await context.Response.WriteAsync("Routed to fallback endpoint");
+app.MapGet("endpoint/function", async context => {
+    await context.Response.WriteAsync("Endpoint Function: It is sunny in LA");
 });
 
 app.Run();
