7baaf5ebc41151b4222d0b58ab2ff52acc460b4b
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-06 00:45:23 +0300

    config 15 prepare

--- Program.cs
+++ Program.cs
@@ -3,28 +3,10 @@ using Platform.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton(typeof(ICollection<>), typeof(List<>));
-
 WebApplication app = builder.Build();
 
-app.MapGet("string", async context =>
-{
-    ICollection<string> collection = context.RequestServices.GetRequiredService<ICollection<string>>();
-    collection.Add($"Request: {DateTime.Now.ToLongTimeString()}");
-    foreach (string str in collection)
-    {
-        await context.Response.WriteAsync($"String: {str}\n");
-    }
-});
-
-app.MapGet("int", async context =>
-{
-    ICollection<int> collection = context.RequestServices.GetRequiredService<ICollection<int>>();
-    collection.Add(collection.Count() + 1);
-    foreach (int val in collection)
-    {
-        await context.Response.WriteAsync($"Int: {val}\n");
-    }
+app.MapGet("/", async context => {
+    await context.Response.WriteAsync("Hello World!");
 });
 
 app.Run();
