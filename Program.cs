b814168782af786a4080c5f7ab0941069cdeb9c7
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-06 01:09:59 +0300

    access config

--- Program.cs
+++ Program.cs
@@ -2,10 +2,21 @@ using Platform;
 using Platform.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var servicesConfig = builder.Configuration;
+// - use configuration settings to set up services
 
 WebApplication app = builder.Build();
+var pipelineConfig = app.Configuration;
+// - use configuration settings to set up pipeline
 
-app.MapGet("/", async context => {
+app.MapGet("config", async (HttpContext context, IConfiguration config) =>
+{
+    string defaultDebug = config["Logging:LogLevel:Default"];
+    await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
+});
+
+app.MapGet("/", async context =>
+{
     await context.Response.WriteAsync("Hello World!");
 });
 
