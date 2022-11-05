0da7323f71dd28c7cf2bf45ca4afe61e49009143
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-05 14:16:58 +0300

    middleware class injection

--- WeatherMiddleware.cs
+++ WeatherMiddleware.cs
@@ -1,19 +1,25 @@
-﻿namespace Platform
+﻿using Platform.Services;
+using System.Runtime.Serialization;
+
+namespace Platform
 {
     public class WeatherMiddleware
     {
         private RequestDelegate next;
+        private IResponseFormatter formatter;
 
-        public WeatherMiddleware(RequestDelegate nextDelegate)
+        public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter respFormatter)
         {
             next = nextDelegate;
+            formatter = respFormatter;
         }
 
         public async Task Invoke(HttpContext context)
         {
             if (context.Request.Path == "/middleware/class")
             {
-                await context.Response.WriteAsync("Middleware Class: It is raining in London");
+                await formatter.Format(context, "Middleware CLASS: It is raining in London");
+                //await context.Response.WriteAsync("Middleware Class: It is raining in London");
             }
             else
             {
