df831b1f1a1dec4472ab5f2211207778c9200b4a
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-05 22:42:11 +0300

    configure services

--- Program.cs
+++ Program.cs
@@ -2,14 +2,28 @@ using Platform;
 using Platform.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
-//builder.Services.AddScoped<IResponseFormatter, GuidService>();
+ConfigurationManager c1 = builder.Configuration;
+IWebHostEnvironment e1 = builder.Environment;
+
+IWebHostEnvironment env = builder.Environment;
+if (env.IsDevelopment())
+{
+    builder.Services.AddScoped<IResponseFormatter, TimeResponseFormatter>();
+    builder.Services.AddScoped<ITimeStamper, DefaultTimeStamper>();
+}
+else
+{
+    builder.Services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
+}
 
 builder.Services.AddScoped<IResponseFormatter, TimeResponseFormatter>();
 builder.Services.AddScoped<ITimeStamper, DefaultTimeStamper>();
 
 WebApplication app = builder.Build();
-//var newScope = app.Services.CreateScope();
+IConfiguration c2 = app.Configuration;
+bool tmp = c1 == c2;
+IWebHostEnvironment e2 = app.Environment;
+tmp = e1 == e2;
 
 app.UseMiddleware<WeatherMiddleware>();
 
@@ -21,18 +35,10 @@ app.MapGet("middleware/function", async (HttpContext context, IResponseFormatter
 //app.MapGet("endpoint/class", WeatherEndpoint.Endpoint2);
 app.MapEndpoint<WeatherEndpoint>("endpoint/class");
 
-//app.MapGet("endpoint/function", async (HttpContext context, IResponseFormatter formatter) =>
-//{
-//    await formatter.Format(context, "Endpoint Function: It is sunny in LA");
-//});
-
 app.MapGet("endpoint/function", async (HttpContext context) =>
 {
-    IResponseFormatter formatter1 = app.Services.CreateScope().ServiceProvider.GetRequiredService<IResponseFormatter>();
-    IResponseFormatter formatter2 = context.RequestServices.GetRequiredService<IResponseFormatter>();
-    bool tmp = formatter1 == formatter2;
-    Console.WriteLine(tmp);
-    await formatter1.Format(context, "Endpoint Function: It is sunny in LA 2");
+    IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+    await formatter.Format(context, "Endpoint Function: It is sunny in LA 2");
 });
 
 app.Run();
