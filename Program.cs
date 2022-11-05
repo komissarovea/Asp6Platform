7041c32555c46af051c50856452a93a19cd8d57f
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-06 02:39:57 +0300

    get environment settings

--- Program.cs
+++ Program.cs
@@ -5,20 +5,25 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 var servicesConfig = builder.Configuration;
 builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
 
+var servicesEnv = builder.Environment;
+// - use environment to set up services
+
 WebApplication app = builder.Build();
 
 var pipelineConfig = app.Configuration;
 // - use configuration settings to set up pipeline
 
+var pipelineEnv = app.Environment;
+// - use envirionment to set up pipeline
+
 app.UseMiddleware<LocationMiddleware>();
 
-app.MapGet("config", async (HttpContext context, IConfiguration config) =>
+app.MapGet("config", async (HttpContext context,
+IConfiguration config, IWebHostEnvironment env) =>
 {
     string defaultDebug = config["Logging:LogLevel:Default"];
     await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
-
-    string environ = config["ASPNETCORE_ENVIRONMENT"];
-    await context.Response.WriteAsync($"\nThe env setting is: {environ}");
+    await context.Response.WriteAsync($"\nThe env setting is: {env.EnvironmentName}");
 });
 
 app.MapGet("/", async context =>
