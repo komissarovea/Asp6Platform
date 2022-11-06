1d9a90fb1166a565c244c076f82c6ef7ba6f54a5
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-06 16:02:59 +0300

    static content

--- Program.cs
+++ Program.cs
@@ -3,21 +3,20 @@ using Microsoft.AspNetCore.HttpLogging;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpLogging(opts =>
-{
-    opts.LoggingFields = HttpLoggingFields.RequestMethod
-        | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
-});
+// builder.Services.AddHttpLogging(opts =>
+// {
+//     opts.LoggingFields = HttpLoggingFields.RequestMethod
+//         | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
+// });
 
 WebApplication app = builder.Build();
 
-// var logger = app.Services
-//     .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+app.UseHttpLogging();
 
-// logger.LogDebug("Pipeline configuration starting");
+app.UseStaticFiles();
+
+app.UseMiddleware<QueryStringMiddleWare>();
 
 app.MapGet("population/{city?}", Population.Endpoint);
-
-// logger.LogDebug(new EventId(77, "my_id"), "Pipeline configuration complete");
 
 app.Run();
