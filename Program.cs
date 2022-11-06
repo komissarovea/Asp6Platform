e287b9ea56c9b67dc572ec5c84e35544725a6d8d
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-06 15:25:08 +0300

    HttpLogging

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
 using Platform;
+using Microsoft.AspNetCore.HttpLogging;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpLogging(opts =>
+{
+    opts.LoggingFields = HttpLoggingFields.RequestMethod
+        | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
+});
+
 WebApplication app = builder.Build();
 
-var logger = app.Services
-    .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+// var logger = app.Services
+//     .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
 
-logger.LogDebug("Pipeline configuration starting");
+// logger.LogDebug("Pipeline configuration starting");
 
 app.MapGet("population/{city?}", Population.Endpoint);
 
-logger.LogDebug(new EventId(77, "my_id"), "Pipeline configuration complete");
+// logger.LogDebug(new EventId(77, "my_id"), "Pipeline configuration complete");
 
 app.Run();
