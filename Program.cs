e89134118536d7403112fd7bf632eb8e5c224e67
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-07 01:43:48 +0300

    session using

--- Program.cs
+++ Program.cs
@@ -1,39 +1,27 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<CookiePolicyOptions>(opts =>
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(opts =>
 {
-    opts.CheckConsentNeeded = context => true;
+    opts.IdleTimeout = TimeSpan.FromMinutes(30);
+    opts.Cookie.IsEssential = true;
 });
 
 WebApplication app = builder.Build();
 
-app.UseCookiePolicy();
+app.UseSession();
+
 app.UseMiddleware<Platform.ConsentMiddleware>();
 
-app.MapGet("/cookie", async context =>
+app.MapGet("/session", async context =>
 {
-    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-    context.Response.Cookies.Append("counter1", counter1.ToString(),
-        new CookieOptions
-        {
-            MaxAge = TimeSpan.FromMinutes(30),
-            IsEssential = true
-        });
-    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-    context.Response.Cookies.Append("counter2", counter2.ToString(),
-        new CookieOptions
-        {
-            //Expires = DateTimeOffset.Now.AddSeconds(3),
-            MaxAge = TimeSpan.FromMinutes(30)
-        });
+    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+    context.Session.SetInt32("counter1", counter1);
+    context.Session.SetInt32("counter2", counter2);
+    await context.Session.CommitAsync();
     await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-});
-app.MapGet("clear", context =>
-{
-    context.Response.Cookies.Delete("counter1");
-    context.Response.Cookies.Delete("counter2");
-    context.Response.Redirect("/");
-    return Task.CompletedTask;
 });
 
 app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
