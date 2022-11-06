8d173d7f18230b8013409c2f1a5c9d1a1de68329
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-06 14:19:58 +0300

    logger category name

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 WebApplication app = builder.Build();
 
-app.Logger.LogDebug("Pipeline configuration starting");
+var logger = app.Services
+    .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+
+logger.LogDebug("Pipeline configuration starting");
 
 app.MapGet("population/{city?}", Population.Endpoint);
 
-app.Logger.LogDebug("Pipeline configuration complete");
+logger.LogDebug(new EventId(77, "my_id"), "Pipeline configuration complete");
 
 app.Run();
