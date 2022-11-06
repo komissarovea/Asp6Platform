50a958bcf333af831a27809a1ce73d7066634300
Komissarov Eugene <komissarovea@hotmail.com>
2022-11-06 16:07:36 +0300

    static custom folder

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
 using Platform;
 using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.FileProviders;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddHttpLogging(opts =>
-// {
-//     opts.LoggingFields = HttpLoggingFields.RequestMethod
-//         | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
-// });
+builder.Services.AddHttpLogging(opts =>
+{
+    opts.LoggingFields = HttpLoggingFields.RequestMethod
+        | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
+});
 
 WebApplication app = builder.Build();
 
 app.UseHttpLogging();
 
 app.UseStaticFiles();
+
+var env = app.Environment;
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
+    RequestPath = "/files"
+});
 
 app.UseMiddleware<QueryStringMiddleWare>();
 
